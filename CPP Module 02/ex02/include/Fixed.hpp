7613958d32541275e320304b4906d0cb53e25a21
Koen Posthuma <kposthum@f0r4s15.codam.nl>
2023-11-04 15:50:52 +0100

    ;_;

---
+++ CPP Module 02/ex02/include/Fixed.hpp
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   Fixed.hpp                                         :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: kposthum <kposthum@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/05/25 12:32:47 by kposthum      #+#    #+#                 */
+/*   Updated: 2023/10/31 17:47:33 by kposthum      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include<iostream>
+#include<iomanip>
+#include<string>
+#include<cmath>
+
+class Fixed
+{
+	private:
+		int rawBits;
+		static const int eight = 8;
+	public:
+		Fixed();
+		Fixed(const Fixed &fix);
+		Fixed(const int i);
+		Fixed(const float f);
+		void operator=(const Fixed& op);
+		bool operator>(const Fixed& op) const;
+		bool operator<(const Fixed& op) const;
+		bool operator>=(const Fixed& op) const;
+		bool operator<=(const Fixed& op) const;
+		bool operator==(const Fixed& op) const;
+		bool operator!=(const Fixed& op) const;
+		float operator+(const Fixed& op) const;
+		float operator-(const Fixed& op) const;
+		float operator*(const Fixed& op) const;
+		float operator/(const Fixed& op) const;
+		Fixed &operator++();
+		Fixed &operator--();
+		float operator++(int);
+		float operator--(int);
+		~Fixed();
+		float toFloat( void ) const;
+		int toInt( void ) const;
+		static Fixed &min(Fixed &a, Fixed &b);
+		static const Fixed &min(const Fixed &a, const Fixed &b);
+		static Fixed &max(Fixed &a, Fixed &b);
+		static const Fixed &max(const Fixed &a, const Fixed &b);
+		void setRawBits(int const raw);
+		int getRawBits(void) const;
+};
+
+std::ostream & operator << (std::ostream &out, const Fixed& op);
+
+#endif
