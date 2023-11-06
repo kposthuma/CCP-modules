7bf101cb8ee459d19e1a5b7f075a1e9281a7a977
Koen <kposthum@student.codam.nl>
2023-11-06 20:58:24 +0100

    ;_;

--- CPP Module 02/ex03/include/Point.hpp
+++ CPP Module 02/ex03/include/Point.hpp
@@ -6,7 +6,7 @@
 /*   By: koen <koen@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/11/04 20:10:21 by koen          #+#    #+#                 */
-/*   Updated: 2023/11/04 20:23:50 by koen          ########   odam.nl         */
+/*   Updated: 2023/11/05 10:14:26 by koen          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,12 @@
 
 class Point{
 	private:
-		const Fixed x;
-		const Fixed y;
+		Fixed const x;
+		Fixed const y;
 	public:
 		Point();
 		Point(const Point& point);
+		Point(const Fixed _x, const Fixed _y);
 		void operator=(const Point& point);
 		~Point();
 		const Fixed getX();
--- CPP Module 02/ex03/src/Point.cpp
+++ CPP Module 02/ex03/src/Point.cpp
@@ -6,27 +6,37 @@
 /*   By: koen <koen@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/11/04 20:10:05 by koen          #+#    #+#                 */
-/*   Updated: 2023/11/04 20:23:02 by koen          ########   odam.nl         */
+/*   Updated: 2023/11/05 10:23:50 by koen          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include<Point.hpp>
 #include<Fixed.hpp>
 
-Point::Point(){
-	x = 0;
-	y = 0;
-}
+Point::Point() : x(0), y(0)
+{}
 
 Point::Point(const Point& point){
 	operator=(point);
 }
 
-void Point::operator=(const Point& point){
-	x = point.getX();
-	y = point.getY();
+Point::Point(const Fixed _x, const Fixed _y): x(_x), y(_y)
+{}
+
+void Point::operator=(const Point& point)
+{
+	x.rawBits = point.x.getRawBits();
+	y.rawBits = point.y.getRawBits();
 }
 
 Point::~Point(){
 
+}
+
+const Fixed Point::getX(){
+	return x;
+}
+
+const Fixed Point::getY(){
+	return y;
 }
