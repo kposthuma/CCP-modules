8188608ec26e8fcaf5d7203d7c8e6a04bede5118
Koen Posthuma <kposthum@f1r5s17.codam.nl>
2023-11-14 13:25:19 +0100

    wut

--- CPP Module 03/ex01/include/ScavTrap.hpp
+++ CPP Module 03/ex01/include/ScavTrap.hpp
@@ -5,21 +5,23 @@
 /*                                                     +:+                    */
 /*   By: kposthum <kposthum@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2023/11/08 16:14:18 by kposthum      #+#    #+#                 */
-/*   Updated: 2023/11/08 17:06:18 by kposthum      ########   odam.nl         */
+/*   Created: 2023/11/14 13:25:01 by kposthum      #+#    #+#                 */
+/*   Updated: 2023/11/14 13:25:02 by kposthum      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
+
 
 #ifndef SCAVTRAP_HPP
 #define SCAVTRAP_HPP
 
 #include<ClapTrap.hpp>
 
-class ScavTrap : ClapTrap {
+class ScavTrap : public ClapTrap {
 	private:
 
 	public:
 		ScavTrap(std::string _name);
+		~ScavTrap();
 		void guardGate();	
 };
 
