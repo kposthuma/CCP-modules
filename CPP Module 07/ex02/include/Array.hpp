7eaf027b465216e753cf0d83cf29c45cc4cd4fe6
Koen Posthuma <kposthum@f1r5s18.codam.nl>
2024-04-23 12:23:23 +0200

    a

--- CPP Module 07/ex02/include/Array.hpp
+++ CPP Module 07/ex02/include/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: kposthum <kposthum@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/01/26 14:26:19 by kposthum      #+#    #+#                 */
-/*   Updated: 2024/04/16 19:20:39 by kposthum      ########   odam.nl         */
+/*   Updated: 2024/04/17 15:19:31 by kposthum      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,42 +30,34 @@ class Array{
 		Array(): _size(0){
 			cont = new T[0];
 		}
-
 		Array(unsigned int n): _size(n){
 			cont = new T[n];
 			for (unsigned int i = 0; i < n; i++){
 				cont[i] = 0;
 			}
 		}
-
 		Array(Array const &src): _size(src.size()){
 			cont = new T[_size];
 			for (unsigned int i = 0; i < _size; i++){
 				cont[i] = (src.getCont())[i];
 			}
 		}
-
 		~Array(){
 			delete []cont;
 		}
-
 		Array			operator=(Array const &src){
 			Array arr(src);
 			return arr;
 		}
-
 		T&				operator[](unsigned int i){
 			if (i < 0 || i >= _size)
 				throw OutOfBoundsException();
 			return cont[i];
 		}
-
 		unsigned int	size() const{
 			return _size;
 		}
-
 		T				*getCont() const{
 			return cont;
 		}
 };
-
