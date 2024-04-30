4fcf2845ed5a9736ac76ca5bb447580933e6f663
Koen Posthuma <kposthum@f0r6s10.codam.nl>
2024-04-30 19:22:37 +0200

    ;_;

--- CPP09/ex02/src/main.cpp
+++ CPP09/ex02/src/main.cpp
@@ -5,18 +5,19 @@
 /*                                                     +:+                    */
 /*   By: kposthum <kposthum@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2024/01/26 14:25:37 by kposthum      #+#    #+#                 */
-/*   Updated: 2024/04/30 16:28:21 by kposthum      ########   odam.nl         */
+/*   Created: 2024/04/30 16:50:39 by kposthum      #+#    #+#                 */
+/*   Updated: 2024/04/30 19:04:47 by kposthum      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-// 00 uses map, 01 uses stack(deque?)
 #include<vector>
 #include<list>
 #include<cstddef>
 #include<string>
 #include<iostream>
 #include<climits>
+#include <algorithm>
+#include <numeric>
 
 template<typename T>
 void insertRange(T &cont, char **arg){
@@ -35,20 +36,44 @@ bool validateInput(char **arg){
 	return true;
 }
 
+void FordJohnsonV(std::vector<int> &vect){
+    std::vector<int>::iterator ptr = vect.begin();
+	int temp;
+	while (ptr < vect.end()){
+		if (ptr + 1 != vect.end()){
+			if (*ptr > *(ptr + 1)){
+				temp = *ptr;
+				*ptr = *(ptr + 1);
+				*(ptr + 1) = temp;
+			}
+		}
+		ptr++;
+		if (ptr != vect.end())
+			ptr++;
+	}
+	std::for_each(vect.begin(), vect.end(), [](const int n){std::cout << n << ' '; });
+    std::cout << '\n';
+}
+
+void FordJohnsonL(std::list<int> &list){
+	(void)list;
+}
+
 int main(int argc, char **argv){
 	if (argc < 3)
 		return 1;
 	if (!validateInput(argv + 1))
 		return 1;
-	std::cout << "input: ";
-	for (size_t i = 1; argv[i]; i++){
-		std::cout << argv[i] << ' ';
-	}
-	std::cout << std::endl;
-	std::vector<int> cont1;
-	std::list<int> cont2;
-	insertRange(cont1, argv + 1);
-	insertRange(cont2, argv + 1);
-	
+	// std::cout << "input: ";
+	// for (size_t i = 1; argv[i]; i++){
+	// 	std::cout << argv[i] << ' ';
+	// }
+	// std::cout << std::endl;
+	std::vector<int> vect;
+	std::list<int> list;
+	insertRange(vect, argv + 1);
+	insertRange(list, argv + 1);
+	FordJohnsonV(vect);
+	FordJohnsonL(list);
 	return 0;
 }
